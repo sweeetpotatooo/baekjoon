b2ef8beaabf8298881af778fbe63fc8943b24369
sweeetpotaooo <sehyun5004@naver.com>
2025-03-14 22:37:33 +0900

    1978

--- 1978.py
+++ 1978.py
@@ -1,4 +1,12 @@
 n = int(input())
-for i in range(n):
-  num =[]
-  f
+num = list(map(int, input().split()))
+count = 0 
+
+for i in num:
+    if i > 1:
+        for j in range(2, i + 1):
+            if i % j == 0:
+                if i == j:
+                    count += 1
+                break
+print(count)
