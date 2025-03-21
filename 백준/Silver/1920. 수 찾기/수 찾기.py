85fbee8c5721799c9bf7a3baaccb57a355649d8e
sweeetpotatooo <86779347+sweeetpotatooo@users.noreply.github.com>
2025-03-21 13:38:09 +0900

    [Silver IV] Title: 수 찾기, Time: 796 ms, Memory: 49136 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,28 @@
+
+import sys
+input = sys.stdin.readline
+
+n= int(input())
+a = list(map(int, input().split()))
+m = int(input())
+num_list = list(map(int, input().split()))
+
+a.sort()
+
+for num in num_list:
+    start, end = 0, n-1
+    is_exist = False
+
+    while start<= end:
+        mid = (start + end) // 2
+        if num == a[mid]:
+            is_exist = True
+            print(1)
+            break
+        elif num > a[mid]:
+            start = mid + 1
+        else:
+            end = mid -1
+
+    if not is_exist:
+        print(0)
