429219618f2a536de0a43daae7b5b7f29dd9a908
sweeetpotatooo <86779347+sweeetpotatooo@users.noreply.github.com>
2025-03-28 14:08:36 +0900

    [Silver I] Title: 트리 순회, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+tree = {}
+
+for _ in range(n):
+    parent, left, right = input().split()
+    tree[parent] = (left, right)
+
+def preorder(node):
+    if node == '.':
+        return ''
+    left, right = tree[node]
+    return node + preorder(left) + preorder(right)
+
+def inorder(node):
+    if node == '.':
+        return ''
+    left, right = tree[node]
+    return inorder(left) + node + inorder(right)
+
+def postorder(node):
+    if node == '.':
+        return ''
+    left, right = tree[node]
+    return postorder(left) + postorder(right) + node
+
+print(preorder('A'))
+print(inorder('A'))
+print(postorder('A'))
