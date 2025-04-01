3103798f7bf9174231113ffcb4b150bc12e05765
sweeetpotaooo <sehyun5004@naver.com>
2025-04-01 10:25:10 +0900

    0331

--- test.py
+++ test.py
@@ -1,29 +1,34 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
+# :받은_편지함_트레이: 입력
+V, E = map(int, input().split())
+edges = []
+for _ in range(E):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+# :아래쪽_화살표: 간선을 비용 기준으로 정렬
+edges.sort()
+# :시계_반대_방향_화살표: 부모 초기화
+parent = [i for i in range(V + 1)]
+# :렌치: 유니온 파인드용 함수들
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])  # 경로 압축
+    return parent[x]
 
-n, m = map(int, input().split())
-graph = {i: [] for i in range(1, n + 1)}
 
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-
-def topol(graph):
-    visited = set()
-    stack = []
-
-    def dfs(node):
-        visited.add(node)
-        for neighbor in graph.get(node, []):
-            if neighbor not in visited:
-                dfs(neighbor)
-        stack.append(node)
-
-    for node in graph:
-        if node not in visited:
-            dfs(node)
-    return stack[::-1]
-
-result = topol(graph)
-print(" ".join(map(str, result)))
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+# :흰색_확인_표시: 크루스칼 핵심
+result = 0
+for cost, a, b in edges:
+    # 사이클이 아니면 MST에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+print(result)
--- 이분_그래프.py
+++ 이분_그래프.py
@@ -12,12 +12,38 @@
 
 # 출력
 # K개의 줄에 걸쳐 입력으로 주어진 그래프가 이분 그래프이면 YES, 아니면 NO를 순서대로 출력한다.
-
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
+def dfs(node, color):
+    colors[node] = color
+    for neighbor in graph[node]:
+        if colors[neighbor] == 0:
+            if not dfs(neighbor, -color):
+                return False
+        elif colors[neighbor] == color:
+            return False
+    return True
+
 k = int(input())
+
 for _ in range(k):
-  v,e = map(int, input().split())
-  
-  
+    v, e = map(int, input().split())
+    graph = [[] for _ in range(v + 1)]
+    for _ in range(e):
+        u, w = map(int, input().split())
+        graph[u].append(w)
+        graph[w].append(u)
+    
+    colors = [0] * (v + 1)
+    bipartite = True
+    for i in range(1, v + 1):
+        if colors[i] == 0:
+            if not dfs(i, 1):
+                bipartite = False
+                break
+    if bipartite:
+        print("YES")
+    else:
+        print("NO")        
