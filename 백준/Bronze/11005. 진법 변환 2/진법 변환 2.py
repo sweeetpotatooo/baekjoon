10f4ee250016251fd13c61f326b1b5df34311c32
sweeetpotatooo <86779347+sweeetpotatooo@users.noreply.github.com>
2025-04-16 10:31:53 +0900

    [Bronze I] Title: 진법 변환 2, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n,b = map(int, input().split())
+x=""
+
+arr="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+while n:
+  x+=str(arr[n%b])
+  n//=b
+print(x[::-1])
