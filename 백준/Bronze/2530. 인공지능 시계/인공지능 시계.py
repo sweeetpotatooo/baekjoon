ae58da3e5e93696491c06ecae9db50c6a8f2eabf
SEHYUN KIM <sehyun5004@naver.com>
2025-05-24 17:01:32 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+H,M,S = map(int,input().split())
+D = int(input().strip())
+
+S += D % 60
+D = D // 60
+if S >= 60:
+    S -= 60
+    M += 1
+
+M += D % 60
+D = D // 60
+if M >= 60:
+    M -= 60
+    H += 1
+
+H += D % 24
+if H >= 24:
+    H -= 24
+
+print(H,M,S)
