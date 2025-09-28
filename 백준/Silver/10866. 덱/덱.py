d879e0615841fa000488ec01e71bf07038b765a4
SEHYUN KIM <sehyun5004@naver.com>
2025-09-28 23:51:49 +0900

    [Silver IV] Title: 덱, Time: 76 ms, Memory: 34944 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,24 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+d = deque()
+n = int(input())
+for _ in range(n):
+    cmd = input().strip().split()
+    if cmd[0] == "push_front":
+        d.appendleft(int(cmd[1]))
+    elif cmd[0] == "push_back":
+        d.append(int(cmd[1]))
+    elif cmd[0] == "pop_front":
+        print(d.popleft() if d else -1)
+    elif cmd[0] == "pop_back":
+        print(d.pop() if d else -1)
+    elif cmd[0] == "size":
+        print(len(d))
+    elif cmd[0] == "empty":
+        print(0 if d else 1)
+    elif cmd[0] == "front":
+        print(d[0] if d else -1)
+    elif cmd[0] == "back":
+        print(d[-1] if d else -1)
+        
