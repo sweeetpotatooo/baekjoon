8493710d9d62f0cc507fd574607f077e13ea60f4
SEHYUN KIM <sehyun5004@naver.com>
2025-10-04 23:35:32 +0900

    [Silver III] Title: 터렛, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -0,0 +1,16 @@
+import sys
+import math
+input = sys.stdin.readline
+T = int(input())
+for _ in range(T):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    d = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    if d == 0 and r1 == r2:
+        print(-1)
+    elif abs(r1 - r2) < d < r1 + r2:
+        print(2)
+    elif d == r1 + r2 or d == abs(r1 - r2):
+        print(1)
+    else:
+        print(0)
+        
